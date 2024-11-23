22e68fcd7f8c8c829f31bf47aa884512d8384f14
sowgand <ramezanzadesogand@gmail.com>
2024-11-23 20:47:21 +0330

    Update test01.js

--- test01.js
+++ test01.js
@@ -1,3 +1,22 @@
-let array =[ 1 , 2 , 3];
-array.push(4);
-console.log(array)
+//calculator
+
+class calculator{
+    add (i,j){
+        return i+j;
+    }
+
+    minus (i,j){
+    return i-j;
+    }
+
+    multiply(i,j){
+        return i*j;
+    }
+
+    devide(i,j){
+        if ( j=== 0){
+            console.log("cannot devide by zero")
+        }else
+        return i/j;
+    }
+}
